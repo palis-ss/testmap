b89a4518cacf18aeee3866bb3ed4ac52b00659be
palis-ss <54706343+palis-ss@users.noreply.github.com>
2022-06-17 18:58:41 +0700

    trying to improve user interface

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,26 +29,41 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(648, 860);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(468, 699);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.Size = new System.Drawing.Size(102, 34);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Marking On";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1004, 668);
+            this.panel1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1503, 939);
+            this.ClientSize = new System.Drawing.Size(1024, 768);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +71,6 @@
         #endregion
 
         private Button button1;
+        private Panel panel1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,77 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+using AxMapWinGIS;
+using MapWinGIS;
+
 namespace testmap
 {
     public partial class Form1 : Form
     {
-        private AxMapWinGIS.AxMap map = new AxMapWinGIS.AxMap();
+        private AxMapWinGIS.AxMap map;
 
         public Form1()
         {
             InitializeComponent();
+            map = new AxMap();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.map.Location = new Point(10, 10);
-            this.map.Size = new System.Drawing.Size(1024, 768);
-            this.Controls.Add(map);
+            panel1.Size = new Size(this.ClientSize.Width - 20, this.ClientSize.Height - 100);
+
+            this.map.Location = new System.Drawing.Point(0, 0);
+            //this.map.Size = new System.Drawing.Size(1024, 768);
+            map.Size = panel1.Size;
+            panel1.Controls.Add(map);
 
             map.CursorMode = MapWinGIS.tkCursorMode.cmPan;
-            map.Projection = MapWinGIS.tkMapProjection.PROJECTION_WGS84;
-            map.TileProvider = MapWinGIS.tkTileProvider.OpenStreetMap;
+            map.Projection = MapWinGIS.tkMapProjection.PROJECTION_GOOGLE_MERCATOR;
+            map.TileProvider = MapWinGIS.tkTileProvider.OpenHumanitarianMap;
             map.SetLatitudeLongitude(13, 100);            
             map.ZoomToTileLevel(8);
+
+            map.SendMouseDown = true;
+            map.MouseDownEvent += map_MouseDown;            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+            panel1.Size = new Size(control.ClientSize.Width-20, control.ClientSize.Height-100);
+            map.Size = panel1.Size;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (map.CursorMode == MapWinGIS.tkCursorMode.cmPan)
+            {
+                map.CursorMode = tkCursorMode.cmNone;
+                button1.Text = "Marking Off";
+            }
+            else
+            {
+                map.CursorMode = tkCursorMode.cmPan;
+                button1.Text = "Marking On";
+            }
+        }
+
+        public void map_MouseDown(object sender, _DMapEvents_MouseDownEvent e)
+        {
+            if (e.button == 1)          // left button
+            {                                
+                MapWinGIS.Point pnt = new MapWinGIS.Point();
+                int handle = map.NewDrawing(tkDrawReferenceList.dlSpatiallyReferencedList);
+                
+                double x = 0.0;
+                double y = 0.0;
+                map.PixelToProj(e.x, e.y, ref x, ref y);
+                pnt.x = x;
+                pnt.y = y;
+                
+                map.DrawCircleEx(handle, pnt.x, pnt.y, 5.0, 255, true);
+                map.Redraw2(tkRedrawType.RedrawMinimal);
+            }
         }
     }
 }
